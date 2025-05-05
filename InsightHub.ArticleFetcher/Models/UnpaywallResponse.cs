62c289d5c73d6ef98ac0f54e3ab40a2cd36dd86e
Christian <chriaas@hiof.no>
2025-05-05 22:20:45 +0200

    Started desentrelizing Json return from UnpayWall

---
+++ InsightHub.ArticleFetcher/Models/UnpaywallResponse.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json.Serialization;
+
+namespace InsightHub.ArticleFetcher.Models
+{
+    public class UnpaywallResponse
+    {
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("year")]
+        public int Year { get; set; }
+
+        [JsonPropertyName("publisher")]
+        public string Publisher { get; set; }
+
+        [JsonPropertyName("doi")]
+        public string Doi { get; set; }
+
+        [JsonPropertyName("doi_url")]
+        public string LandingPageUrl { get; set; }
+
+        [JsonPropertyName("z_authors")]
+        public List<Author> Authors { get; set; }
+
+        [JsonPropertyName("best_oa_location")]
+        public BestOALocation BestOALocation { get; set; }
+    }
+
+    public class Author
+    {
+        [JsonPropertyName("given")]
+        public string Given { get; set; }
+
+        [JsonPropertyName("family")]
+        public string Family { get; set; }
+    }
+
+    public class BestOALocation
+    {
+        [JsonPropertyName("url_for_pdf")]
+        public string UrlForPdf { get; set; }
+
+    }
+}
--- InsightHub.ArticleFetcher/Services/UnpaywallService.cs
+++ InsightHub.ArticleFetcher/Services/UnpaywallService.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net.Http;
 using System.Threading.Tasks;
+using System.Text.Json;
+using InsightHub.ArticleFetcher.Models;
 
 namespace InsightHub.ArticleFetcher.Services
 {
@@ -13,31 +15,42 @@ namespace InsightHub.ArticleFetcher.Services
             _httpClient = httpClient;
         }
 
-        public async Task<string> GetRawArticlesAsync(string query)
+        public async Task<ArticleDto> GetRawArticlesAsync(string query)
         {
-            // TEMP: Using a real DOI
             var doi = "10.1093/biosci/biz088";
             var url = $"https://api.unpaywall.org/v2/{doi}?email=mytrados@gmail.com";
 
-            // log outgoing request
-            Console.WriteLine($"Sending GET request to: {url}");
 
+            Console.WriteLine($"Sending GET request to: {url}");
             var response = await _httpClient.GetAsync(url);
 
             if (!response.IsSuccessStatusCode)
             {
                 Console.WriteLine($"Failed with status: {response.StatusCode}");
-                return $"Error: {response.StatusCode}";
+                return null;
             }
 
-            // Log the raw JSON response
             var content = await response.Content.ReadAsStringAsync();
-            Console.WriteLine("Response received:");
-            Console.WriteLine(content);
+            Console.WriteLine("Response received");
 
-            return content;
+            var article = JsonSerializer.Deserialize<UnpaywallResponse>(content);
+
+            if (article == null)
+                return null;
+
+            var dto = new ArticleDto
+            {
+                Title = article.Title,
+                Year = article.Year,
+                Publisher = article.Publisher,
+                Doi = article.Doi,
+                LandingPageUrl = article.LandingPageUrl,
+                PdfUrl = article.BestOALocation?.UrlForPdf,
+                Authors = article.Authors?.Select(a => $"{a.Given} {a.Family}").ToList()
+            };
+
+            return dto;
         }
-
 
     }
 }
